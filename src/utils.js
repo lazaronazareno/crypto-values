74904d96227506c2da8822961d6d4efc0690662e
lazaronazareno <lazaronazareno@gmail.com>
2023-01-05 17:34:39 -0300

    list of available coins

---
+++ src/utils.js
@@ -0,0 +1,22 @@
+export const Coins = [
+  {
+    cod: 'USD',
+    name: 'Dolar Estadounidense'
+  },
+  {
+    cod: 'ARS',
+    name: 'Peso Argentino'
+  },
+  {
+    cod: 'MXN',
+    name: 'Peso Mexicano'
+  },
+  {
+    cod: 'EUR',
+    name: 'Euro'
+  },
+  {
+    cod: 'GBP',
+    name: 'Libra Esterlina'
+  }
+]
